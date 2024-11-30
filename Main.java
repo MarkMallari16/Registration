b8f5e7bd1125364dbcf79cf5b95cc1371b57455d
Mark Christian Mallari <104248911+MarkMallari16@users.noreply.github.com>
2024-12-01 00:12:08 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package registration;
 public class Main {
 
     public static void main(String[] args) {
-       Registration reg = new Registration();
-       reg.show();
+       Login log = new Login();
+       log.show();
     }
 }
--- Registration.java
+++ Registration.java
@@ -467,7 +467,7 @@ public class Registration extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Registration().setVisible(true);
+//                new Registration().setVisible(true);
             }
         });
     }
