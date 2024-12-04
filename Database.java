17d72b7737529dcd86d5b5f2460febd2431582fe
Mark Christian Mallari <104248911+MarkMallari16@users.noreply.github.com>
2024-12-04 22:04:53 +0800

    Add files via upload

--- Database.java
+++ Database.java
@@ -3,5 +3,6 @@ package registration;
 import java.util.HashMap;
 
 public class Database {
+
     public static HashMap<Integer, String[]> hm = new HashMap<Integer, String[]>();
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 package registration;
 
-public class Main {
+public class Main {
+
     public static void main(String[] args) {
        Login log = new Login();
        log.show();
